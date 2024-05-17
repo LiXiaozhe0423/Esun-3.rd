4adf4ddaf7c21ce5ca6afbeecd982348bc2b8282
LiXiaozhe0423 <bule2043@gmail.com>
2024-05-17 17:50:00 +0800

    增加type

---
+++ Esun.3rd/src/assets/type.ts
@@ -0,0 +1,8 @@
+interface parking {
+    seq: number;
+    area: string;
+    name: string;
+    count: number;
+    address: string;
+
+}
--- Esun.3rd/src/components/Navbar.vue
+++ Esun.3rd/src/components/Navbar.vue
@@ -3,25 +3,43 @@ import { ref } from 'vue';
 import axios from 'axios';
 const responseData = ref([]);
 
+//資料發查邏輯
 async function example() {
-  console.log('AAAAAAAAAAAAAAAAAAAAA')
   try {
     const response = await axios.get('https://api.kcg.gov.tw/api/service/Get/897e552a-2887-4f6f-a6ee-709f7fbe0ee3');
-    console.log(response.data);
-    responseData.value = response.data;
+    const parkingData: parking[] = response.data.data.map((item: any) => ({
+      seq: item.seq,
+      area: item['行政區'],
+      name: item['臨時停車處所'],
+      count: item['可提供小型車停車位'],
+      address: item['地址']
+    }));
+    if (selectedDistrict.value != '') {
+      const filterdate: parking[] = filterData(parkingData);
+      console.log(filterdate);
+    }
   } catch (error) {
     console.error('出事了 阿北!!!!!!!!!!!!!!!!!', error);
   }
 }
 
+//清單呈現邏輯
+const selectedDistrict = ref('');
 const districts = ref([
   "前金", "新興", "鹽埕", "左營", "楠梓", "鼓山", "旗津", "苓雅", "三民", "前鎮",
   "小港", "鳳山", "鳥松", "大社", "仁武", "大樹", "岡山", "燕巢", "梓官", "永安",
   "彌陀", "橋頭", "田寮", "茄萣", "阿蓮", "路竹", "湖內", "那瑪夏", "桃源", "茂林",
-  "六龜", "美濃", "旗山", "甲仙", "內門", "杉林", "林園", "大寮區"
+  "六龜", "美濃", "旗山", "甲仙", "內門", "杉林", "林園", "大寮"
 ]);
 
-const selectedDistrict = ref('');
+
+function filterData(parkingData: parking[]) {
+  return parkingData.filter(item =>
+    item.area.includes(selectedDistrict.value)
+  );
+
+}
+
 </script>
 
 <template>
@@ -44,10 +62,12 @@ const selectedDistrict = ref('');
             </select>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <div>
+          <input class="form-control me-2" placeholder="Search">
+        </div>
+        <div>
+          <button class="btn btn-outline-success" @click="example()">Search</button>
+        </div>
       </div>
     </div>
   </nav>
